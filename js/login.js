204dd8d5c357633395fbaac05e6af82af2c04a5c
JoosangKim <fable0831@gmail.com>
2022-02-20 23:27:39 +0900

    to-do 기본 틀
    1. 시계
    2. login(local strage 저장)
    3. quotes
    4. to-do list
    5. background 구현

---
+++ js/login.js
@@ -0,0 +1,33 @@
+//local storage login
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN = "hidden";
+const USERNAME_KEY = "myUsername"
+
+function onLoginSubmit(e) {
+    e.preventDefault();
+    loginForm.classList.add(HIDDEN);
+    const username = loginInput.value;
+    localStorage.setItem("USERNAME_KEY", username);
+    paintGteetings(username);
+};
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+function paintGteetings(username) {
+    greeting.innerText = `You still have enough time ${username}`;
+    greeting.classList.remove(HIDDEN);
+}
+
+const savedUsername = localStorage.getItem("USERNAME_KEY");
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGteetings(savedUsername);
+};
+
+//TO-DO 로그아웃
---
+++ js/quotes.js
@@ -0,0 +1,49 @@
+const quotes = [
+    {
+        quote: "Here's Johnny!",
+        movie: "The Shining (1980)",
+    },
+    {
+        quote: "I see dead people.",
+        movie: "The Sixth Sense (1999)",
+    },
+    {
+        quote: "I'm scared to close my eyes, I'm scared to open them!",
+        movie: "The Blair Witch Project (1999)",
+    },
+    {
+        quote: "What an excellent day for an exorcism.",
+        movie: "The Exorcist (1973)",
+    },
+    {
+        quote: "I Wanna Play a Game. Let the Games Begin.",
+        movie: "Saw (2004)",
+    },
+    {
+        quote: "Hi, I'm Chucky. Wanna play?",
+        movie: "Child's Play (1988)"
+    },
+    {
+        quote: "I'm your number one fan.",
+        movie: "Misery (1990)"
+    },
+    {
+        quote: "Well, Clarice, have the lambs stopped screaming?",
+        movie: "The Silence of the Lambs(1991)"
+    },
+    {
+        quote: "I Mean, I told you not to go in that house.",
+        movie: "Get Out(2017)"
+    },
+    {
+        quote: "We all go a little mad sometimes.",
+        movie: "Psycho(1960)"
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const movie = document.querySelector("#quote span:last-child");
+const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+quote.innerText = todaysQuote.quote;
+movie.innerText = `- ${todaysQuote.movie}`;

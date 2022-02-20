339ca7ccf4ad77d1febe6f1a797fc50522b5fe2f
JoosangKim <fable0831@gmail.com>
2022-02-21 04:11:24 +0900

    css 2차 수정
    1. 음악 재생 기능 추가
    2. checkbox를 활용한 물 마시기 확인 추가(아이콘 변경)
    3. 위치 확인 후 날씨 위치 정보 추가

--- js/login.js
+++ js/login.js
@@ -4,30 +4,30 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN = "hidden";
-const USERNAME_KEY = "myUsername"
+const USERNAME_KEY = "myUsername";
 
 function onLoginSubmit(e) {
-    e.preventDefault();
-    loginForm.classList.add(HIDDEN);
-    const username = loginInput.value;
-    localStorage.setItem("USERNAME_KEY", username);
-    paintGteetings(username);
-};
+  e.preventDefault();
+  loginForm.classList.add(HIDDEN);
+  const username = loginInput.value;
+  localStorage.setItem("USERNAME_KEY", username);
+  paintGteetings(username);
+}
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
 function paintGteetings(username) {
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEN);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN);
 }
 
 const savedUsername = localStorage.getItem("USERNAME_KEY");
 
 if (savedUsername === null) {
-    loginForm.classList.remove(HIDDEN);
-    loginForm.addEventListener("submit", onLoginSubmit);
+  loginForm.classList.remove(HIDDEN);
+  loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    paintGteetings(savedUsername);
-};
+  paintGteetings(savedUsername);
+}
 
 //TO-DO 로그아웃
--- js/todo.js
+++ js/todo.js
@@ -16,34 +16,35 @@ function deleteToDo(event) {
 }
 
 function paintToDo(newTodo) {
-    const li = document.createElement("li");
-    li.id = newTodo.id;
-    const span = document.createElement("span");
-    span.innerText = newTodo.text;
-    const button = document.createElement("button");
-    button.textContent = "❌\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0";
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
-    toDoList.appendChild(li);
-  }
-  
-  function handleToDoSubmit(event) {
-    event.preventDefault();
-    const newTodo = toDoInput.value;
-    toDoInput.value = "";
-    const newTodoObj = {
-      text: newTodo,
-      id: Date.now(),
-    };
-    toDos.push(newTodoObj);
-    paintToDo(newTodoObj);
-    saveToDos();
-  }
-  toDoForm.addEventListener("submit", handleToDoSubmit);
-  const savedToDos = localStorage.getItem(TODOS_KEY);
-  if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
-  }
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.textContent =
+    "❌\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0\u00a0";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  const newTodoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
+  saveToDos();
+}
+toDoForm.addEventListener("submit", handleToDoSubmit);
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
+}
